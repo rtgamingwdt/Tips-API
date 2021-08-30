51f84e8669a5bebd5c3dbaced188f3851e99f81c
RT <46906497+rtgamingwdt@users.noreply.github.com>
2021-08-30 18:35:28 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+express().listen(3000);
+
+express().get("/", (req, res) => {
+  let api = require('./factions/tips.json');
+  let keys = Object.keys(api.tips).map(elem => parseInt(elem, 10));
+  let random = Math.floor(Math.random() * Math.max(...keys));
+  res.send(api.tips[random]);
+});
