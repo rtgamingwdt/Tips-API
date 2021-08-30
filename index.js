bd5fd3d7dc70f608331504acde9ab7f08dea8fae
RT <46906497+rtgamingwdt@users.noreply.github.com>
2021-08-30 19:04:13 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-
 const express = require('express');
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/factions", (req, res) => {
   let api = require('./factions/tips.json');
   let keys = Object.keys(api.tips).map(elem => parseInt(elem, 10));
   let random = Math.floor(Math.random() * Math.max(...keys));
